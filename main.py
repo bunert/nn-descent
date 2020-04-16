81f852db5f52c5741fbd298b2b7ea4def7c9b6ba
Dan <dkluser@student.ethz.ch>
2020-04-16 19:22:25 +0200

    download audio.data if not there

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import subprocess
 from dataset import GaussianDataset, AudioDataset
 from nearestneighbors import reference_nearest_neighbors, nearest_neighbors
 
+from pathlib import Path
+import urllib.request
+
+my_file = Path("audio.data")
+if not my_file.is_file():
+    print("audio.data not here, downloading...")
+    urllib.request.urlretrieve ("http://kluser.ch/audio.data", "audio.data")
+
 # dataset = GaussianDataset(10,10000,100)
 dataset = AudioDataset()
 
