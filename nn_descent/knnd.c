81e21041a62c31c7f228639c5f5b869b32aef5bd
Dan <dkluser@student.ethz.ch>
2020-04-24 18:04:08 +0200

    fused sample,reverse,union. considerable speedup

--- nn_descent/knnd.c
+++ nn_descent/knnd.c
@@ -28,6 +28,7 @@ int make_test_data(dataset_t* data, int n, int d)
 vec_t* heap_list_create(int size, int k)
 {
     vec_t* hl = malloc(sizeof(vec_t) * size);
+    hl->size = size;
     if (!hl) return NULL;
 
     for (int i = 0; i < size; i++) {
@@ -74,21 +75,16 @@ vec_t* nn_descent(dataset_t data, float(*metric)(float*, float*, int), int k, fl
         printf("error: neighborhood size must be less than dataset size\n");
         return NULL;
     }
-    //printf("beginning nn descent...\n");
 
     vec_t* B     = heap_list_create(data.size, k);
     vec_t* old   = heap_list_create(data.size, k);
-    vec_t* old_r = heap_list_create(data.size, k);
     vec_t* new   = heap_list_create(data.size, k);
-    vec_t* new_r = heap_list_create(data.size, k);
 
-    if (!B || !old || !old_r || !new || !new_r) {
+    if (!B || !old || !new) {
         printf("error: failed to allocate one or more heap lists\n");
         if (B) heap_list_free(B, data.size);
         if (old) heap_list_free(old, data.size);
-        if (old_r) heap_list_free(old_r, data.size);
         if (new) heap_list_free(new, data.size);
-        if (new_r) heap_list_free(new_r, data.size);
         return NULL;
     }
 
@@ -103,59 +99,48 @@ vec_t* nn_descent(dataset_t data, float(*metric)(float*, float*, int), int k, fl
     }
 
     int c;
-    int pk = k * rho;
-    int stop_iter = delta * data.size * k;
-    vec_t sample_buf;
-    vec_create(&sample_buf, pk);
+    int stop_iter = 0.1*delta * data.size * k;
     do {
-        vec_clear(old);
-        vec_clear(new);
-        for (int v = 0; v < data.size; v++) {   // TODO: parallel
-            for (int j = 0; j < B[v].size; j++) {
-                if (!(B[v].arr[j].new) && B[v].arr[j].id != v) {
-                    heap_insert(&old[v], &B[v].arr[j]);
-                }
-            }
-            sample_neighbors(&new[v], &B[v], pk, true);
-        }
+        heap_list_free(old, data.size);
+        heap_list_free(new, data.size);
+        old   = heap_list_create(data.size, k);
+        new   = heap_list_create(data.size, k);
 
-        reverse_heap_list(old_r, old, data.size);
-        reverse_heap_list(new_r, new, data.size);
+        sample_reverse_union(new, old, B, k, data.size);
 
         c = 0;
         for (int v = 0; v < data.size; v++) {   // TODO: parallel
-            sample_neighbors(&sample_buf, &old_r[v], pk, false);
-            if (!heap_union(&old[v], &sample_buf)) return NULL;
-
-            sample_neighbors(&sample_buf, &new_r[v], pk, false);
-            if (!heap_union(&new[v], &sample_buf)) return NULL;
             for (int i = 0; i < new[v].size; i++) {
-                for (int j = 0; j < new[v].size; j++) {
-                    if (i == j) continue;
+                for (int k=0; k<2; k++) {
+                    vec_t* heap_list = (k==0) ? new : old;
 
-                    node_t u1, u2;
-                    u1.id = new[v].arr[i].id;
-                    u2.id = new[v].arr[j].id;
+                    for (int j = 0; j < heap_list[v].size; j++) {
+                        if (i == j) continue;
 
-                    if (u1.id <= u2.id || heap_find_by_index(&old[v], u2.id) == -1) continue;
-                    float l = metric(data.values[u1.id], data.values[u2.id], data.dim);
-                    u1.val = u2.val = l;
-                    u1.new = u2.new = true;
-                    c += nn_update(&B[u1.id], &u2);
-                    c += nn_update(&B[u2.id], &u1);
-                }
+                        node_t u1, u2;
+                        u1.id = new[v].arr[i].id;
+                        u2.id = heap_list[v].arr[j].id;
+
+                        if (u1.id <= u2.id) continue; //|| heap_find_by_index(&old[v], u2.id) == -1) continue;
+                        // smh never get here...
+
+                        float l = metric(data.values[u1.id], data.values[u2.id], data.dim);
+
+                        u1.val = u2.val = l;
+                        u1.new = u2.new = true;
+                        c += nn_update(&B[u1.id], &u2);
+                        c += nn_update(&B[u2.id], &u1);
+                    }
+                }    
             }
         }
-        //printf("iteration complete: %d / %d\n", c, stop_iter);
+       // printf("iteration complete: %d / %d\n", c, stop_iter);
     } while (c >= stop_iter);
     printf("NNDescent finished \n");
     //printf("done, cleaning up...\n");
 
-    vec_free(&sample_buf);
     heap_list_free(old, data.size);
-    heap_list_free(old_r, data.size);
     heap_list_free(new, data.size);
-    heap_list_free(new_r, data.size);
 
     return B;
 }
@@ -175,23 +160,64 @@ int reverse_heap_list(vec_t* dst, vec_t* src, int size)
     return 0;
 }
 
-int sample_neighbors(vec_t* dst, vec_t* src, int k, bool use_new)
+int heap_insert_bounded(vec_t* h, node_t* node, int k)
 {
-    if (src->size == 0) return 0;
-
-    vec_clear(dst);
-
-    int j = 0;
-    for (int i = 0; i < src->size && j < k; i++) {
-        int rand_index = rand() % src->size;
-        node_t t = src->arr[rand_index];
-        if (use_new && !t.new) continue;
-        if (use_new) {
-            src->arr[rand_index].new = false;
-            t.new = false;
-        }
-        if (heap_insert(dst, &t)) j++;
+    if (h->size<k) {
+        return heap_insert(h, node);
+    } else {
+        if (heap_find_by_index(h, node->id) >= 0)
+            return 0;
+        float curr_max = h->arr[0].val;
+        if (node->val > curr_max)
+            h->arr[0] = *node;
+        max_heapify(h, 0); 
+        return 1;
     }
 
-    return j;
 }
+
+int sample_reverse_union(vec_t* new, vec_t* old, vec_t* B, int k, int N) {
+    // this function samples, reverses and does the union all at once
+    for (int i=0; i<N; i++) {
+        vec_t heap = B[i];
+        int u = i;
+        node_t *arr = heap.arr;
+
+        for (int j=0; j<heap.size; j++) {
+            node_t n = arr[j];
+            int v = n.id;
+            float rand_weight = rand()/(float)RAND_MAX;
+
+            // depending on flag select new or old heaplist
+            vec_t* heap_list = (n.new) ? new : old;
+            
+            // TODO: check if heap_list addressable like array with id !!
+            (heap_insert_bounded(&heap_list[u], & (node_t) {v, rand_weight, n.new}, k));
+            // reverse (add to B[v] if rand_weight amongst the K smallest...)
+            (heap_insert_bounded(&heap_list[v], & (node_t) {u, rand_weight, n.new}, k));
+        }
+    }
+
+
+    // set sampled to false 
+    for (int i=0; i<N; i++) {
+        vec_t heap = new[i];
+        int u = i;
+        node_t *arr = heap.arr;
+        // printf("\nnode %d: ", u);
+        for (int j=0; j<heap.size; j++) {
+            node_t n = arr[j];
+            int v = n.id;
+            // printf("%d ", v);
+            int index = heap_find_by_index(&B[u], v);
+            if (index >= 0)
+                B[u].arr[index].new = false;
+
+            index = heap_find_by_index(&B[v], u);
+            if (index >= 0)
+                B[v].arr[index].new = false;
+        }
+    }
+    return 0;
+}
+
